1fce4ff5257a6e87059bea25e6f785d03a2ecf82
Paulo Vitor <paulo@zim.local>
2010-04-13 21:52:54 -0300

    drawing the player as a square

--- src/org/gonp/Player.java
+++ src/org/gonp/Player.java
@@ -10,29 +10,31 @@ import javax.microedition.khronos.opengles.GL10;
 public class Player {
 	private FloatBuffer vertexBuffer;
 	private ShortBuffer indexBuffer;
-	private float angle = 0;
+	private float[] coordinates = { 
+			1, -1, 0,
+			0, -1, 0,
+			0, 0, 0,
+			0, 0, 0,
+			1, -1, 0,
+			1, 0, 0
+	};
+	private short[] indexes = {0,1,2,3,4,5};
 
 	public Player(GL10 gl) {
-		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(3*3*4);
+		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(coordinates.length*4);
 		byteBuffer.order(ByteOrder.nativeOrder());
 		vertexBuffer = byteBuffer.asFloatBuffer();
-		vertexBuffer.put(new float[] { 
-			-0.5f, -0.5f, 0f, // (x1, y1, z1)
-			0.5f, -0.5f, 0f, // (x2, y2, z2)
-			0f, 0.5f, 0f // (x3, y3, z3)
-		});
+		vertexBuffer.put(coordinates);
 		vertexBuffer.position(0);
 		
-		ByteBuffer booBuffer = ByteBuffer.allocateDirect(3*2);
+		ByteBuffer booBuffer = ByteBuffer.allocateDirect(indexes.length*2);
 		booBuffer.order(ByteOrder.nativeOrder());
 		indexBuffer = booBuffer.asShortBuffer();
-		indexBuffer.put(new short[] {0,1,2});
+		indexBuffer.put(indexes);
 		indexBuffer.position(0);
 	}
 
 	public void render(GL10 gl) {
-		gl.glRotatef(angle += 0.001, 0f, 1f, 0f);
-		
 	    // set the color of our element
 	    gl.glColor4f(0f, 0.5f, 0f, 0.5f);
 	 
@@ -40,6 +42,6 @@ public class Player {
 	    gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
 	 
 	    // finally draw the vertices
-	    gl.glDrawElements(GL10.GL_TRIANGLES, 3, GL10.GL_UNSIGNED_SHORT, indexBuffer);
+	    gl.glDrawElements(GL10.GL_TRIANGLES, coordinates.length/3, GL10.GL_UNSIGNED_SHORT, indexBuffer);
 	}
 }
