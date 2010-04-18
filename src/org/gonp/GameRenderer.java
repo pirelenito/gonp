5bb49013e0c2cee351d7d6672f5d14c6906dbefd
Paulo Vitor <paulo@zim.local>
2010-04-18 16:34:57 -0300

    closes #6 Drawing two players on the screen.

--- src/org/gonp/GameRenderer.java
+++ src/org/gonp/GameRenderer.java
@@ -6,14 +6,16 @@ import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLSurfaceView.Renderer;
 
 public class GameRenderer implements Renderer {
-	private final Player player = new Player();
+	private final Player player1 = new Player();
+	private final Player player2 = new Player();
 		
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		gl.glClearColor(1, 0, 0, 1);
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		player.render(gl);
+		player1.render(gl);
+		player2.render(gl);
 	}
 
 	@Override
@@ -24,5 +26,6 @@ public class GameRenderer implements Renderer {
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 	    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+	    player2.setY(-.9f);
 	}
 }
