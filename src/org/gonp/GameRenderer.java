f8059c43379ee70d93b820cc9780177760467aac
pelizza <pelizza@antenor.local>
2010-05-04 21:05:08 -0300

    refs #10 * Ball now moves on the screen.

--- src/org/gonp/GameRenderer.java
+++ src/org/gonp/GameRenderer.java
@@ -12,33 +12,10 @@ public class GameRenderer implements Renderer {
 	private final VertexObject player2 = VertexObjectFactory.createSquare();
 	private final VertexObject ball = VertexObjectFactory.createCircle();
 	private int width;
+	private PointF ballVelocity = new PointF(.031f, .017f);
 		
-	@Override
-	public void onDrawFrame(GL10 gl) {
-		update();
-		render(gl);
-	}
-
-	private void render(GL10 gl) {
-		gl.glLoadIdentity();
-		gl.glTranslatef(-1f, -1f, 0);
-		gl.glScalef(2, 2, 1);
-		
-		gl.glClearColor(1, 0, 0, 1);
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		player1.render(gl);
-		player2.render(gl);
-		ball.render(gl);
-	}
-
-	private void update() {
-		
-	}
-
-	@Override
-	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		this.width = width;
-		gl.glViewport(0, 0, width, height);
+	public void onTouchEvent(MotionEvent event) {
+		player1.setPosition(new PointF(event.getX()/width, player1.getPosition().y));
 	}
 
 	@Override
@@ -52,7 +29,36 @@ public class GameRenderer implements Renderer {
 	    ball.setScale(new PointF(.02f, .02f));
 	}
 
-	public void onTouchEvent(MotionEvent event) {
-		player1.setPosition(new PointF(event.getX()/width, player1.getPosition().y));
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		this.width = width;
+		gl.glViewport(0, 0, width, height);
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		update();
+		render(gl);
+	}
+
+	private void update() {
+		PointF newPosition = new PointF(ball.getPosition().x + ballVelocity.x, ball.getPosition().y + ballVelocity.y);
+		
+		if (newPosition.x > 1 || newPosition.x < 0) ballVelocity.x *= -1;
+		if (newPosition.y > 1 || newPosition.y < 0) ballVelocity.y *= -1;
+		
+		ball.setPosition(newPosition);
+	}
+
+	private void render(GL10 gl) {
+		gl.glLoadIdentity();
+		gl.glTranslatef(-1f, -1f, 0);
+		gl.glScalef(2, 2, 1);
+		
+		gl.glClearColor(1, 0, 0, 1);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		player1.render(gl);
+		player2.render(gl);
+		ball.render(gl);
 	}
 }
